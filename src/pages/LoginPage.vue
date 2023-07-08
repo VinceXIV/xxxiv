394ac6a927a3513134b78d17ba0cea7f90d26afc
Vincent <makokhavomondi@gmail.com>
2023-07-08 13:32:58 +0300

    Create the login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,26 @@
 <template>
     <div class="login-page">
-        <p>Login page</p>
+        <div class="container">
+            <h1 class="form-title">
+                College Registration Course
+            </h1>
+
+            <form>
+                <h2 class="login">Login</h2>
+
+                <div class="form-item">
+                    <label for="email-address">Email address</label>
+                    <input name="email-address" placeholder="email address"/>
+                </div>
+    
+                <div class="form-item">
+                    <label for="password">Password</label>
+                    <input name="password" placeholder="password" />
+                </div>
+    
+                <input class="btn" type="submit" value="enter system">
+            </form>
+        </div>
     </div>
 </template>
 
@@ -12,8 +32,57 @@ export default({
 
 <style scoped>
 .login-page {
-    background-color: blue;
     width: 100vw;
     height: 100vh;
+    display: grid;
+    place-items: center;
+}
+
+.login-page .container {
+    outline: var(--outline-bold);
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 30rem;
+}
+
+.form-title {
+    font-size: 1.3rem;
+    text-align: center;
+}
+
+.login-page form {
+    padding: 0.8rem;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0.7rem;
+}
+
+form .login {
+    font-size: 1rem;
+    margin-bottom: 0.5rem;
+}
+
+.form-item {
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+    width: 100%;
+    justify-content: left;
+}
+
+.form-item input {
+    outline: var(--outline-faint);
+    padding: 0.3rem;
+}
+
+@media screen and (max-width: 40rem){
+    .login-page .container {
+        width: 80vw;
+    }
 }
 </style>
