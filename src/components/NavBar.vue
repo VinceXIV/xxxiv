b594b7a49a4db38fcf9cefbe985506ba62bd5f6f
Vincent <makokhavomondi@gmail.com>
2023-07-09 23:51:47 +0300

    Create the navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav>
+        <div class="container">
+            <h1>Code Challenge</h1>
+            <button class="btn" @click="changeLoginState">
+                {{ token? 'Logout' : 'Login' }}
+            </button>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default({
+    name: "NavBar",
+
+    props: {
+        login: Function,
+        logout: Function,
+        loggedIn: Boolean
+    },
+
+    methods: {
+        changeLoginState: function(){
+            if(this.loggedIn){
+                this.logout()
+            }else {
+                this.login()
+            }
+        }
+    }
+
+})
+</script>
+
+<style>
+nav {
+    width: 100vw;
+    height: 10vh;
+    display: grid;
+    place-items: center;
+    background-color: rgb(0, 119, 190, 1);
+}
+
+nav .container {
+    display: flex;
+    flex-direction: row;
+    margin: auto;
+    width: 80%;
+    height: max-content;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+}
+</style>
