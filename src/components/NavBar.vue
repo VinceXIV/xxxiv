e2026511407a69c813cec1cd50dd56ce156707db
Vincent <makokhavomondi@gmail.com>
2023-07-10 20:45:02 +0300

    Hide some navbar page links for people who aren't logged in

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,8 @@
         <div class="container">
             <h1 class="icon" @click="navigate('/home')">Code Challenge</h1>
             <ul class="actions">
-                <li class="btn" :class="pageState('/home')" @click="navigate('/home')">Home</li>
-                <li class="btn" :class="pageState('/register')" @click="navigate('/register')">Register</li>
+                <li v-if="loggedIn" class="btn" :class="pageState('/home')" @click="navigate('/home')">Home</li>
+                <li v-if="loggedIn" class="btn" :class="pageState('/register')" @click="navigate('/register')">Register</li>
                 <li class="btn" :class="pageState('/login')"  @click="changeLoginState">
                     {{ loggedIn ? 'Logout' : 'Login' }}
                 </li>
