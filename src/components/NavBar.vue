2d2f74fc538574780121a8aca509c3d3e11861ff
Vincent <makokhavomondi@gmail.com>
2023-07-11 13:28:57 +0300

    Fix an issue where the "login" button is active even when one clicks on "home" while they are logged in

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,7 @@
             <h1 class="icon" @click="navigate('/home')">Code Challenge</h1>
             <ul class="actions">
                 <li v-if="loggedIn" class="btn"
+                    :key="currentPath"
                     :class="pageState('/home')"
                     @click="navigate('/home')">
                     Home
